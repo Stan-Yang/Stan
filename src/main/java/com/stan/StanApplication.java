f194482ca62daf82888a1a5ed20e827102ee359e
Stan-Yang <49123553+Stan-Yang@users.noreply.github.com>
2020-06-28 17:19:15 +0800

    11

--- src/main/java/com/stan/StanApplication.java
+++ src/main/java/com/stan/StanApplication.java
@@ -1,14 +1,13 @@
 package com.stan;
 
-import com.stan.filter.AccessRecorderFilter;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 //自动扫描mapper包
